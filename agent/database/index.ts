00d71fb50828b149afaca84d09206773a92899af
kamalbuilds <geniusamansingh@gmail.com>
2025-02-01 11:14:27 +0530

    change the db to supabase

--- agent/database/index.ts
+++ agent/database/index.ts
@@ -1,5 +1,6 @@
 import { PostgresDatabaseAdapter } from "@elizaos/adapter-postgres";
 import { SqliteDatabaseAdapter } from "@elizaos/adapter-sqlite";
+import { SupabaseDatabaseAdapter } from "@elizaos/adapter-supabase";
 import Database from "better-sqlite3";
 import path from "path";
 
@@ -13,7 +14,9 @@ export function initializeDatabase(dataDir: string) {
     const filePath =
       process.env.SQLITE_FILE ?? path.resolve(dataDir, "db.sqlite");
     // ":memory:";
-    const db = new SqliteDatabaseAdapter(new Database(filePath));
+    const supabaseurl = "https://ddrfwgtbujyjtylthpzq.supabase.co";
+    const supabasekey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImRkcmZ3Z3RidWp5anR5bHRocHpxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3MzgxMjU1MzQsImV4cCI6MjA1MzcwMTUzNH0.41GFuRv9EHrvmjmuJNlfHaq0OaxC0EV9bolCIZ0YCYI";
+    const db = new SupabaseDatabaseAdapter(supabaseurl, supabasekey);
     return db;
   }
 }
