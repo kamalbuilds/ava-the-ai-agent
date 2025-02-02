7942fd5a7d60e6d2442e05bc96e3d9eec18b9175
kamalbuilds <geniusamansingh@gmail.com>
2025-02-02 16:32:15 +0530

    rm kestra and .devcontainer

--- .devcontainer/configure-ports.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-echo "Configuring ports..."
-
-gh codespace ports visibility 3000:public -c $CODESPACE_NAME
-gh codespace ports visibility 9650:public -c $CODESPACE_NAME
-
-echo "Ports configuration completed!"
