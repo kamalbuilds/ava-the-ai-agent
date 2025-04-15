42a43984788290d2ee8409a4d47f37bb78bd2007
kamalbuilds <geniusamansingh@gmail.com>
2025-04-15 11:05:18 +0800

    Add module declarations and Solver trait to lib.rs

--- runeswap-solver/src/lib.rs
+++ runeswap-solver/src/lib.rs
@@ -1,6 +1,12 @@
 // Basic module structure for RuneSwap Solver
 // This will be expanded in future commits
 
+// Module declarations
+pub mod types; // Will be added in future commits
+pub mod config; // Will be added in future commits
+pub mod runeswap; // Will be added in future commits
+pub mod solver; // Will be added in future commits
+
 /// Main entry point for the RuneSwap NEAR Intents integration
 pub struct RuneSwapSolver {
     // Will be expanded with configuration and client fields
@@ -11,6 +17,12 @@ impl RuneSwapSolver {
     pub fn new() -> Self {
         Self { }
     }
+}
+
+/// Solver trait that will be implemented by different solver strategies
+pub trait Solver {
+    fn process_intent(&self) -> Result<(), Box<dyn std::error::Error>>;
+    fn execute_swap(&self) -> Result<(), Box<dyn std::error::Error>>;
 }
 
 // Export modules (to be added in future commits) 
